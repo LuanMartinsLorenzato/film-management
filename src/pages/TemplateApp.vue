c69fb46ed2ff4a42311fb670488fe036943809ee
Luan Lorenzato <57531302+LuanMartinsLorenzato@users.noreply.github.com>
2024-02-29 15:37:39 -0300

    Create Organism Header

--- src/pages/TemplateApp.vue
+++ src/pages/TemplateApp.vue
@@ -1,8 +1,14 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <main>
+    <OrganismHeader />
   </main>
 </template>
+
+<script setup lang="ts">
+import OrganismHeader from '@/components/organisms/OrganismHeader.vue';
+
+  
+</script>
+
+<style scoped>
+</style>
